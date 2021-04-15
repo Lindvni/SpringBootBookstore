9fad0ae09a7b3720536685789be097dd4f3b338c
LINDANI <lindvni@gmail.com>
2021-04-15 22:34:40 +0200

    2nd commit

---
+++ src/main/java/com/example/LibrarySystem/controller/bookController.java
@@ -0,0 +1,49 @@
+package com.example.LibrarySystem.controller;
+
+import com.example.LibrarySystem.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.awt.print.Book;
+
+@Controller
+public class bookController {
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping("/")
+    public String viewHomePage(Model model) {
+        model.addAttribute("listEmployees", bookService.getAllbook());
+        return "index";
+    }
+
+    @GetMapping("/new")
+    public String showNewBookForm(Model model) {
+        // create model attribute to bind form data
+        Book book = new Book();
+        model.addAttribute("book", book);
+        return "new_book";
+    }
+
+//    @PostMapping("/saveBook")
+//    public String saveBook(@ModelAttribute("book") Book book) {
+//        // save employee to database
+//        bookService.saveBook(book);
+//        return "redirect:/";
+//    }
+//    @GetMapping("/showFormForUpdate/{id}")
+//    public String showFormForUpdate(@PathVariable(value = "id") long id, Model model) {
+//
+//        // get employee from the service
+//        Book book = bookService.getBookById(id);
+//        // set employee as a model attribute to pre-populate the form
+//        model.addAttribute("book", book);
+//        return "update_book";
+//    }
+
+}
