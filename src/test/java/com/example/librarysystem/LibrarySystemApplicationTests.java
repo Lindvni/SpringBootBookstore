ff032f381851a91ddde45e18527dd12c91bc8ea6
Michael Wiles <michaelwiles@lumkani.com>
2021-04-19 10:50:56 +0200

    added tests and also added an unfinished test... to validate a book is saved (read book and check it is saved)

--- src/test/java/com/example/librarysystem/LibrarySystemApplicationTests.java
+++ src/test/java/com/example/librarysystem/LibrarySystemApplicationTests.java
@@ -1,15 +1,45 @@
 package com.example.librarysystem;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.ActiveProfiles;
+
+import com.example.librarysystem.model.Book;
 
 @ActiveProfiles("test")
 @SpringBootTest
 class LibrarySystemApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @PersistenceContext
+    EntityManager em;
+    
+    
+    @Test
+    @Transactional
+    public void SaveBook() {
+        Book book = new Book();
+        em.persist(book);
+    }
+    @Test
+    @Transactional
+    public void When_SaveBook_BookIsSaved() {
+        Book book = new Book();
+        String title = "book's title";
+        book.setTitle(title);
+        em.persist(book);
+        
+        // add code to read book and check it is there
+
+        fail();
+    }
+    
+    
 
 }
